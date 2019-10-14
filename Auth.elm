44303c48e111000513936cce4aedd3a0f49982c8
jgordinier <jg@allmail.net>
2019-10-14 13:03:44 -0500

    Update Auth.elm

--- Auth.elm
+++ Auth.elm
@@ -4,12 +4,19 @@ module Auth exposing (spaceId, accessToken, postContentTypeId)
 
 
 spaceId =
-    ""
-
+    "hw72depcww1p"
 
 accessToken =
-    ""
-
+    "eKA89MMvMEWIQ06NU-9776_qr-vX3gCWr_j8lRdFJyo"
 
 postContentTypeId =
-    ""
+    "events"
+
+personalToken =
+    "CFPAT-YWRq6x20_7QecU_OBvy2plW1WYKLIpjESCOmsjj2hfQ"
+
+defaultAuthorId =
+    "author"
+
+authorContentTypeId =
+    "6xzqgoXrCzXrkwU7vfOPzN"
